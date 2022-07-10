cc5f6f839c4634b7e2335acff9ae28df64ac826a
sanskar <sanskarjainskj@gmail.com>
2022-07-10 11:23:47 +0530

    removed bug: now link should work

--- src/components/MainPage/Portfolio.jsx
+++ src/components/MainPage/Portfolio.jsx
@@ -19,7 +19,13 @@ export default function Portfolio({isActive}){
                 <PortfolioItem link="https://sans1812.pythonanywhere.com/quiz/" img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.ea99aIBs_QQSITPRoMWZvwHaD4%26pid%3DApi&f=1"/>
                 <PortfolioItem link="https://sans1812.pythonanywhere.com/explo/sans/" img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.XYbEaZF3-qRmSY8PpPqbhAHaE8%26pid%3DApi&f=1"/>
                 <PortfolioItem link="https://sans1812.pythonanywhere.com/problemset/" img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.explicit.bing.net%2Fth%3Fid%3DOIP.jMLSvZ0rhLV7YuZRBrruyQHaD4%26pid%3DApi&f=1"/>
-                <Link to="/sudoku"><PortfolioItem link="" img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdarksudoku.com%2Fassets%2Fimages%2Fgame-board.png&f=1&nofb=1"/></Link>
+                <Link to="/sudoku">
+                    <div class="portfolio-item">
+                        <div class="image">
+                            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdarksudoku.com%2Fassets%2Fimages%2Fgame-board.png&f=1&nofb=1" alt=""/>
+                        </div>
+                    </div>
+                </Link>
                 <PortfolioItem img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Fh8JCeH4VMUotEO-VWV1rwAAAA%26pid%3DApi&f=1"/>
                 <PortfolioItem img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.Fh8JCeH4VMUotEO-VWV1rwAAAA%26pid%3DApi&f=1"/>    
             </div>
